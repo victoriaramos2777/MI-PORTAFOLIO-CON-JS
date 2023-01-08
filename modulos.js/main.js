d61e87722c12f171342e60ce713d9f4d5bbb7dd5
Victoria Ramos <777vikiramos22@gmail.com>
2023-01-08 17:33:20 -0600

    agregue cambios en la nav cree cambios en scss y agregue evento en js para los enlaces y el menu y las animaciones

--- modulos.js/main.js
+++ modulos.js/main.js
@@ -1,1 +1,11 @@
+const menu= document.querySelector('.hamburg');
+const enlaces = document.querySelector('.enlaces-menu');
+const barras = document.querySelectorAll('.hamburg span');
 
+
+
+menu.addEventListener('click', () => {
+    enlaces.classList.toggle('activado');
+    barras.forEach(child => {child.classList.toggle('animado')});
+    menu.classList.toggle('girar');
+});
