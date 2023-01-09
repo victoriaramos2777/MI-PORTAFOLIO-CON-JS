259d9cd12ade43bac17ee8c47bfb13287e7f8e53
Victoria Ramos <777vikiramos22@gmail.com>
2023-01-09 14:53:40 -0600

    cambie la forma de la animacion de las barras de menu

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# MI-REPOSITORIO
+# MI-Portafolio.
--- modulos.js/main.js
+++ modulos.js/main.js
@@ -1,12 +1,9 @@
 //constantes para el menu hamburguesa y enlaces de la nav
-const menu= document.querySelector('.hamburg');
-const enlaces = document.querySelector('.enlaces-menu');
-const barras = document.querySelectorAll('.hamburg span');
+const menu = document.getElementById('hamburg');
 
 
 //evento escucha para agregar la aniamcion de los br 
 menu.addEventListener('click', () => {
-    enlaces.classList.toggle('activado');
-    barras.forEach(child => {child.classList.toggle('animado')});
-    menu.classList.toggle('girar');
-});
+    menu.classList.toggle('active');
+    
+});   
