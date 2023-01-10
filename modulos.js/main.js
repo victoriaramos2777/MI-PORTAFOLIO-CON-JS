be22dab42f97d4e347f003b748d1a6c2a5ee76d4
Victoria Ramos <777vikiramos22@gmail.com>
2023-01-09 21:25:56 -0600

    corregi error de nav

--- modulos.js/main.js
+++ modulos.js/main.js
@@ -1,9 +1,14 @@
 //constantes para el menu hamburguesa y enlaces de la nav
 const menu = document.getElementById('hamburg');
-
+const enlacesMenu = document.getElementById('enlaces-menu');
 
 //evento escucha para agregar la aniamcion de los br 
 menu.addEventListener('click', () => {
     menu.classList.toggle('active');
+    if(menu === "active"){
+       menu = "enlacesMenu".classList.toggle('enlaces-menu');
+    }
     
-});   
+    
+    
+});
