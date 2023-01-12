d78fbe2f063452c7699de17386a5ebbe19fb066d
Victoria Ramos <777vikiramos22@gmail.com>
2023-01-12 08:51:00 -0600

    fuencionalidad de la form con javascript agregada

--- modulos.js/main.js
+++ modulos.js/main.js
@@ -1,16 +1,45 @@
+
 //constantes para el menu hamburguesa y enlaces de la nav
-let menu = document.querySelector('#hamburg');
-const enlacesMenu = document.querySelector('.enlaces-menu');
+const hamburg= document.querySelector('.hamburg');
+const enlaces = document.querySelector('.enlaces-menu');
+const barras = document.querySelectorAll('.hamburg span');
+
+//const para el efecto typing
 
 //evento escucha para agregar la aniamcion de los br 
 
-menu.addEventListener('click',()=>{
-    menu.classList.toggle('active');
-   
-    if(menu.classList.contains("active")){
-        enlacesMenu.classList.add('enlaces-menu');
-    }else{
-        enlacesMenu.classList.remove('enlaces-menu');
-    }
-})
+hamburg.addEventListener('click', () => {
+    enlaces.classList.toggle('activado');
+    barras.forEach(child => {child.classList.toggle('active')});
+    hamburg.classList.toggle('girar');
 
+});
+
+//const para llamar la form
+const form = document.getElementById("form");
+
+
+
+form.addEventListener( "submit" , (event) =>{
+    event.preventDefault();
+
+    document.write("<h1>Gracias por su mensaje</h1>");
+    
+    });
+
+document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("form").addEventListener('submit',validarForm);
+});
+function validarForm(e){
+    e.preventDefault();
+    let name = document.getElementById("mensaje").value;
+    if (name.length === 0) {
+       alert('No ha escrito nada') ;
+       return;
+    }
+    
+} 
+    
+
+let reCorto = /\S+@\S+\.\S+/
+reCorto.test('prueba@correo.com');
